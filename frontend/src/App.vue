7642a6feecc4ab43484d5eb7a9f27a79cc857d15
Ilia Muravev <degtyarev327@gmail.com>
2023-12-09 01:02:23 +0300

    remove unused

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,17 +3,6 @@
 </template>
 
 <script lang="ts" setup>
-import { onMounted } from 'vue'
-import { getAllStocks } from './api'
-
-async function fetchAndSaveStocks() {
-  const allStocks = await getAllStocks()
-  console.log('Список всех акций:', allStocks)
-}
-
-onMounted(() => {
-  // fetchAndSaveStocks()
-})
 </script>
 
 <style>
