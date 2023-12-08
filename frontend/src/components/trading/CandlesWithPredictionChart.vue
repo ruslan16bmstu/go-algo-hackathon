bf861a0755cd3476ffc43ccb1727018cc8101d30
Ilia Muravev <degtyarev327@gmail.com>
2023-12-09 01:51:14 +0300

    remove stub data from candles chart

--- frontend/src/components/trading/CandlesWithPredictionChart.vue
+++ frontend/src/components/trading/CandlesWithPredictionChart.vue
@@ -5,45 +5,24 @@
 <script setup lang="ts">
 import { ref, onMounted } from 'vue'
 import { createChart, ISeriesApi } from 'lightweight-charts'
-
-// Типы данных для свечей и прогноза
-interface CandleData {
-  time: number;
-  open: number;
-  high: number;
-  low: number;
-  close: number;
-}
+import { CandleData } from '../../api'
 
 interface PredictionData {
-  time: number;
-  value: number;
+  time: string
+  value: number
 }
 
 const chart = ref<HTMLElement | null>(null)
 let candleSeries: ISeriesApi<'Candlestick'> | null = null
 let predictionSeries: ISeriesApi<'Line'> | null = null
 
+const props = defineProps<{
+  candles: CandleData[]
+  prediction: PredictionData[]
+}>()
+
 onMounted(() => {
   if (chart.value) {
-    const candleData: CandleData[] = [
-      {close: 4976.16, high: 4977.99, low: 4970.12, open: 4972.89, time: 1587660000000},
-      {close: 4977.33, high: 4979.94, low: 4971.34, open: 4973.20, time: 1587660060000},
-      {close: 4977.93, high: 4977.93, low: 4974.20, open: 4976.53, time: 1587660120000},
-      {close: 4966.77, high: 4968.53, low: 4962.20, open: 4963.88, time: 1587660180000},
-      {close: 4961.56, high: 4972.61, low: 4961.28, open: 4961.28, time: 1587660240000},
-      {close: 4964.19, high: 4964.74, low: 4961.42, open: 4961.64, time: 1587660300000},
-      {close: 4968.93, high: 4972.70, low: 4964.55, open: 4966.96, time: 1587660360000},
-      {close: 4979.31, high: 4979.61, low: 4973.99, open: 4977.06, time: 1587660420000},
-      {close: 4977.02, high: 4981.66, low: 4975.14, open: 4981.66, time: 1587660480000},
-      {close: 4985.09, high: 4988.62, low: 4980.30, open: 4986.72, time: 1587660540000},
-      {close: 4985.09, high: 4988.62, low: 4980.30, open: 4986.72, time: 1587660540000},
-    ] // Замените этот массив своими данными для свечей
-    const predictionData: PredictionData[] = [
-      {time: 1587660940000, value: 5000},
-      {time: 1587661340000, value: 5040},
-    ] // Замените этот массив своими данными для прогноза
-
     const priceChart = createChart(chart.value, {
       width: chart.value.clientWidth,
       height: chart.value.clientHeight,
@@ -57,7 +36,7 @@ onMounted(() => {
       wickVisible: true,
     })
 
-    candleSeries.setData(candleData)
+    candleSeries.setData(props.candles)
 
     // Создание линии прогноза
     predictionSeries = priceChart.addLineSeries({
@@ -65,7 +44,7 @@ onMounted(() => {
       lineWidth: 1,
     })
 
-    predictionSeries.setData(predictionData.map(point => ({
+    predictionSeries.setData(props.prediction.map(point => ({
       time: point.time,
       value: point.value,
     })))
